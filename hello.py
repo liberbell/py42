3a634d96f42a9c07bc1179950f50c82a6f1d96a3
Liberbell <liberbell@gmail.com>
2023-05-06 14:20:15 +0900

    hello4

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 print("hello")
 
-print(a = 4)
-print(b = 5)
+a = 4
+b = 5
 c = [6, 7, 8]
 
 d = "Hi"
@@ -10,4 +10,7 @@ d = "Hi"
 def area(x):
     return x**2
 
+print(a)
+print(type(a))
+print(b)
 print(area(3))
