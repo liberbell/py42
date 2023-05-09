becf0b2302eeb6cd19e3559f0792df49901eee43
Liberbell <liberbell@gmail.com>
2023-05-10 08:39:49 +0900

    normmethods2

--- normmethods.py
+++ normmethods.py
@@ -2,3 +2,10 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def point_in_rec(self, lowleft, upright):
+        print("I am ordinary")
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
