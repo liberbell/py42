928d43b820b773f9468fbd14a960dc3ff5c5cec9
Liberbell <liberbell@gmail.com>
2023-05-10 08:25:35 +0900

    methods3

--- methods.py
+++ methods.py
@@ -1,11 +1,16 @@
-class Point:
+# class Point:
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
 point1 = Point(7, 8)
 print(point1.x, point1.y)
 
 print("john".count("j"))
 print("john".capitalize())
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
