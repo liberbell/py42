01dba6484c53cf0cdc7bb019b8db672d1ac8950f
Liberbell <liberbell@gmail.com>
2023-05-10 08:27:43 +0900

    methods5

--- methods.py
+++ methods.py
@@ -16,4 +16,7 @@ class Point:
         self.y = y
 
     def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0] < self.x < upright[0] 
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
