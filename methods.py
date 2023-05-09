064f7fdc51b88d87eb942ab7695b4fdbfb4e7c5a
Liberbell <liberbell@gmail.com>
2023-05-10 08:26:59 +0900

    methods4

--- methods.py
+++ methods.py
@@ -14,3 +14,6 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def falls_in_rectangle(self, lowleft, upright):
+        if lowleft[0] < self.x < upright[0] 
