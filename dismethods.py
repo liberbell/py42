41bd87b82d6dfe3bc82e9665582e86e869bda25a
Liberbell <liberbell@gmail.com>
2023-05-11 08:26:21 +0900

    dismethods1

---
+++ dismethods.py
@@ -0,0 +1,15 @@
+class Point:
+    def __init__(self, x, y):
+        print("Hi! I am __init__!no")
+        self.x = x
+        self.y = y
+
+    def point_in_rec(self, lowleft, upright):
+        print("I am ordinary")
+        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+            return True
+        else:
+            return False
+        
+    def distance_from_point(self, x, y)
+        return ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
