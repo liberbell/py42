348bc4eeed2712bfcbb1ca17d14e348455e2e1e6
Liberbell <liberbell@gmail.com>
2023-05-12 08:35:10 +0900

    wrapup12

--- wrapup1.py
+++ wrapup1.py
@@ -3,3 +3,10 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.lowleft < self.x < rectangle.upright.x \
+        and rectangle.lowleft.y < self.y < rectangle.upright.y:
+            return True
+        else:
+            return False
