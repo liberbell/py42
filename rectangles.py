77015168da782b3a3b572e2d49bbcf4ea286db96
Liberbell <liberbell@gmail.com>
2023-05-12 08:25:45 +0900

    rectangles2

--- rectangles.py
+++ rectangles.py
@@ -5,7 +5,8 @@ class Point:
         self.y = y
 
     def falls_in_rectangle(self, rectangle):
-        if rectangle.lowleft.x < self.x < rectangle.upright.x and rectangle.lowleft.y < self.y < rectangle.upright.y:
+        if rectangle.lowleft.x < self.x < rectangle.upright.x \
+            and rectangle.lowleft.y < self.y < rectangle.upright.y:
             return True
         else:
             return False
