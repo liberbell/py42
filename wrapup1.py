adb497d65e1bea227a276b198dfb49005b03eb9e
Liberbell <liberbell@gmail.com>
2023-05-12 09:00:30 +0900

    wrapup19

--- wrapup1.py
+++ wrapup1.py
@@ -5,7 +5,7 @@ class Point:
         self.y = y
 
     def falls_in_rectangle(self, rectangle):
-        if rectangle.lowleft < self.x < rectangle.upright.x \
+        if rectangle.lowleft.x < self.x < rectangle.upright.x \
         and rectangle.lowleft.y < self.y < rectangle.upright.y:
             return True
         else:
