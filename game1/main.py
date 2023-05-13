adc5f4ffd53e53fa1f685efcf18152d92a5f4426
Liberbell <liberbell@gmail.com>
2023-05-13 12:00:17 +0900

    main2

--- game1/main.py
+++ game1/main.py
@@ -1,1 +1,14 @@
 from random import randint
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+            and rectangle.point1.y < self.y < rectangle.point2.y:
+            return True
+        else:
+            return False
