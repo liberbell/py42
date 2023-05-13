e2d53d5098e03f40e4ad7fd1f2fdceb5f5c0b090
Liberbell <liberbell@gmail.com>
2023-05-13 12:08:09 +0900

    main5

--- game1/main.py
+++ game1/main.py
@@ -25,3 +25,11 @@ class Rectangle:
     
 rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
                       Point(randint(10, 19), randint(10, 19)))
+
+print("Rectangle cordinates: ",
+      rectangle.lowleft.x, ",",
+      rectangle.lowleft.y, "and",
+      rectangle.upright.x, ",",
+      rectangle.upright.y)
+
+user_point = Point(float(input("Guess x: ")), float("Guess y: "))
