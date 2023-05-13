69635880c4d34410bb0a96410c599e385cefdfbb
Liberbell <liberbell@gmail.com>
2023-05-13 12:03:33 +0900

    main3

--- game1/main.py
+++ game1/main.py
@@ -12,3 +12,13 @@ class Point:
             return True
         else:
             return False
+        
+class Rectangle:
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+               (self.point2.y - self.point1.y)
