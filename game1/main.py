6d139fb0876551b7092318963929183a9421c185
Liberbell <liberbell@gmail.com>
2023-05-13 12:10:33 +0900

    main2

--- game1/main.py
+++ game1/main.py
@@ -27,10 +27,10 @@ rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
                       Point(randint(10, 19), randint(10, 19)))
 
 print("Rectangle cordinates: ",
-      rectangle.lowleft.x, ",",
-      rectangle.lowleft.y, "and",
-      rectangle.upright.x, ",",
-      rectangle.upright.y)
+      rectangle.point1.x, ",",
+      rectangle.point2.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
 
 user_point = Point(float(input("Guess x: ")), float("Guess y: "))
 user_area = float(input("Guess rectangle area: "))
