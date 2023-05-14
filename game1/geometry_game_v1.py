c626789ec83b2b61e25a83651d95f66dd568d336
Liberbell <liberbell@gmail.com>
2023-05-14 22:51:07 +0900

    geometry_game_v12

--- game1/geometry_game_v1.py
+++ game1/geometry_game_v1.py
@@ -27,6 +27,20 @@ class Rectangle:
 class GuiRectangle(Rectangle):
     
     def draw(self, canvas):
+        myturtle.penup()
+        myturtle.goto(50, 75)
+
+        myturtle.pendown()
+        myturtle.forward(100)
+        myturtle.left(90)
+        myturtle.forward(200)
+        myturtle.left(90)
+
+        myturtle.forward(100)
+        myturtle.left(90)
+        myturtle.forward(200)
+
+        turtle.done()
 
 Gui_rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)),Point(randint(10, 400), randint(10, 400)))
 
